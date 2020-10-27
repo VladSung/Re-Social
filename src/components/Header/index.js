0a7f6d838856f648bf7c68defafeaa79f930cbe6
Vlad Sung <shutov.vladislav.2017@gmail.com>
2020-10-27 13:13:57 +0400

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <img src='https://i.pinimg.com/236x/48/a2/11/48a21167d141a926c43755b7d8309b76.jpg' />
+                <p>Re: Social</p>
+            </header>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+    return (
+        <main>
+            <img src='https://i.pximg.net/img-original/img/2020/10/27/00/00/06/85262746_p0.jpg' />
+        </main>
+    );
+}
+
+export default Main;
